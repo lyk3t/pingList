5179ddf06cf6eef4f55ee0fe711d5c491b207eeb
dom-mages <dominic.mages@gmail.com>
2018-05-24 00:30:58 +0200

    finished for wanted usecase

--- pingAll.sh
+++ pingAll.sh
@@ -3,24 +3,37 @@
 #Simple Bash script to ping a list of servers and only output on fail
 #(c) 2018
 
+RED='\033[0;31m'
+NC='\033[0m' # No Color
+timestamp() {
+  date +"%T"
+} 
+
 declare -A HOSTLIST
+
+# List of hosts to be pinged 
 HOSTLIST[PC]=192.168.0.199 
 HOSTLIST[Router]=192.168.0.1
 HOSTLIST[DUMMY]=192.168.0.2
-# echo 192.168.0.199 192.168.0.1 | xargs -n1 ping -w 1 | grep -b1 100   
+# echo 192.168.0.199 192.168.0.1 | xargs -n1 ping -w 1 | grep -b1 100  
 
-echo Pinging hosts...
+echo Pinging hosts in loop...
+echo Press CTRL + C to interrupt the script
+echo
+echo Following hosts will be pinged:
 for K in "${!HOSTLIST[@]}"
 do 
-    echo Pinging $K - ${HOSTLIST[$K]}
+    echo $K - ${HOSTLIST[$K]}...
 done
 
-for K in "${!HOSTLIST[@]}"
-do 
-    ping -c 1 -W 1 ${HOSTLIST[$K]} > /dev/null
-    if [ $? -eq 0 ]; then
-    echo "node $output is up" 
-    else
-    echo "node $output is down"
-    fi
+while :
+do
+    for K in "${!HOSTLIST[@]}"
+    do 
+        ping -c 1 -W 1 ${HOSTLIST[$K]} > /dev/null
+        if [ $? != 0 ]; then
+        echo -e "$(timestamp): ${RED}$K: Host not reachable!${NC}" 
+        fi
+    done
+    sleep 1;
 done
